fceee4d47dce0920bd4aa5115cec5d6809cc9d57
sp01\claudio.koji <claudio.koji@totvs.com.br>
2017-10-07 16:53:21 -0300

    Unificacao widgets

--- fluig/wcm/widget/widgetFoT/src/main/resources/view.ftl
+++ fluig/wcm/widget/widgetFoT/src/main/resources/view.ftl
@@ -12,37 +12,145 @@
 </style>
 <div id="widgetFoT_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide"
 	data-params="widgetFoT.instance({instanceId: '${instanceId!''}',widgetURI: '${widgetURI!''}',mode: '${mode!''}'})">
-	<div class="panel panel-primary">
-	    <div class="panel-heading">
-	        <h3 class="panel-title">Fluig of Things</h3>
-	    </div>
-	    <div class="panel-body">
-	    	<div class="row fot-input">
-	    		<div class="col-sm-5">
-	    			<div class="form-group">
-				        <label for="gatilho">Gatilho</label>
-				        <select class="form-control" id="gatilho_${instanceId}" name="gatilho_${instanceId}">
-				        	<option></option>
-						</select>
+	
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="panel panel-primary">
+			    <div class="panel-heading">
+			        <h3 class="panel-title">Fluig of Things</h3>
+			    </div>
+			    <div class="panel-body">
+			    	<div class="row fot-input">
+			    		<div class="col-sm-5">
+			    			<div class="form-group">
+						        <label for="gatilho">Gatilho</label>
+						        <select class="form-control" id="gatilho_${instanceId}" name="gatilho_${instanceId}">
+						        	<option></option>
+								</select>
+						    </div>
+			    		</div>
+			    		<div class="col-sm-2 va-center" align="center">
+			    			<span class="fluigicon fluigicon-arrow-right fluigicon-xl"></span>
+			    		</div>
+			    		<div class="col-sm-5">
+			    			<div class="form-group">
+						        <label for="acao">A&ccedil;&atilde;o</label>
+						        <select class="form-control" id="acao_${instanceId}" name="acao_${instanceId}">
+						        	<option></option>
+								</select>
+						    </div>
+			    		</div>
+			    	</div>
+				    <div align="center">
+				    	<button type="button" class="btn btn-primary" data-fot-add>Incluir</button>
 				    </div>
-	    		</div>
-	    		<div class="col-sm-2 va-center" align="center">
-	    			<span class="fluigicon fluigicon-arrow-right fluigicon-xl"></span>
-	    		</div>
-	    		<div class="col-sm-5">
-	    			<div class="form-group">
-				        <label for="acao">A&ccedil;&atilde;o</label>
-				        <select class="form-control" id="acao_${instanceId}" name="acao_${instanceId}">
-				        	<option></option>
-						</select>
-				    </div>
-	    		</div>
-	    	</div>
-		    <div align="center">
-		    	<button type="button" class="btn btn-primary" data-fot-add>Incluir</button>
+			    </div>
 		    </div>
-	    </div>
-    </div>
+		</div>
+	</div>
+	
+	<div class="row">
+	
+	<div class="col-sm-6">
+		<div class="panel panel-primary">
+		    <div class="panel-heading">
+		        <h3 class="panel-title">Cadastro de Gatilhos</h3>
+		    </div>
+		    <div class="panel-body">
+		        <form role="form">
+				    <div class="form-group">
+				        <label for="nome">Nome</label>
+				        <input type="text" class="form-control" id="nome_${instanceId}" placeholder="Digite o nome do gatilho">
+				    </div>
+				    <div class="row">
+				    	<div class="col-sm-6">
+					    	<div class="form-group">
+						        <label for="categoria">Categoria</label>
+						        <select class="form-control" id="categoria_${instanceId}" name="categoria_${instanceId}">
+						        	<option></option>
+								    <option value="sensores">Sensores</option>
+								    <option value="workflows">Workflows</option>
+								    <option value="documentos">Documentos</option>
+								</select>
+						    </div>
+				    	</div>
+				    	<div class="col-sm-6">
+					    	<div class="form-group">
+						        <label for="tipo">Tipo</label>
+						        <select class="form-control" id="tipo_${instanceId}">
+								</select>
+						    </div>
+				    	</div>
+				    </div>
+				    
+				    <button type="button" class="btn btn-default" data-gatilho-add>Incluir</button>
+				</form>
+		    </div>
+		</div>
+	</div>
+	
+	<div class="col-sm-6">
+		<div class="panel panel-primary">
+		    <div class="panel-heading">
+		        <h3 class="panel-title">Cadastro de Ações</h3>
+		    </div>
+		    <div class="panel-body">
+		        <form role="form" onSubmit="data-aprovador-add">
+				    <div class="form-group">
+				        <label for="nome">Nome</label>
+				        <input type="text" class="form-control" id="nomeAc_${instanceId}" placeholder="Digite o nome da ação" required>
+				    </div>
+				    <div class="row">
+				    	<div class="col-sm-6">
+				    		<div class="form-group">
+						        <label for="categoria">Categoria</label>
+						        <select class="form-control" id="categoriaAc_${instanceId}" required>
+						        	<option></option>
+								    <option value="sensores">Atuador</option>
+								    <option value="workflow">Workflow</option>
+								    <option value="documento">Documento</option>
+								</select>
+						    </div>
+				    	</div>
+				    	<div class="col-sm-6">
+							<div class="form-group">
+						        <label for="tipo">Tipo</label>
+						        <select class="form-control" id="tipoAc_${instanceId}" required>
+								    					    
+								</select>
+						    </div>			    	
+				    	</div>
+				    </div>
+				    
+				    <button type="button" class="btn btn-default" data-acao-add>Incluir</button>
+				    <script>
+				    	$("#categoriaAc_${instanceId}").change(function() {
+							var $cat = $(this);
+							var key = $cat.val();
+							var vals = [];
+							switch(key) {
+								case 'sensores':
+									vals = ['Ligar', 'Desligar'];
+									break;
+								case 'workflow':
+									vals = ['Iniciar', 'Movimentar'];
+									break;
+								case 'documento':
+									vals = ['Criar', 'Aprovar'];
+							}
+							var $tipo = $("#tipoAc_${instanceId}");
+							$tipo.empty();
+							$.each(vals, function(index, value) {
+								$tipo.append("<option>" + value + "</option>");
+							});
+						});
+				    </script>
+				</form>
+		    </div>
+		</div>
+	</div>
+	
+	</div>
 </div>
 
 <script src="/webdesk/vcXMLRPC.js" type="text/javascript"></script>
--- fluig/wcm/widget/widgetFoT/src/main/webapp/resources/js/widgetFoT.js
+++ fluig/wcm/widget/widgetFoT/src/main/webapp/resources/js/widgetFoT.js
@@ -1,21 +1,73 @@
 var widgetFoT = SuperWidget.extend({
 	instanceId: null,
 	widgetURI: null,
+	formGatilho: 12,
+	formAcao: 6,
+	
 
     //método iniciado quando a widget é carregada
     init: function() {
     	var $this = this;
-    	widgetGatilhos.widgetURI = this.widgetURI;
-    	widgetGatilhos.instanceId = this.instanceId;
+    	widgetFoT.widgetURI = this.widgetURI;
+    	widgetFoT.instanceId = this.instanceId;
 
-        this.loadComboboxDS("gatilho_" + this.instanceId, "dsGatilhos", "nome", "nome", null);
+        this.initFoT();
+        this.initGatilhos();
+    },
+    
+    initFoT: function() {
+    	this.loadComboboxDS("gatilho_" + this.instanceId, "dsGatilhos", "nome", "nome", null);
         this.loadComboboxDS("acao_" + this.instanceId, "dsAcoes", "nome", "nome", null);
     },
+    
+    initGatilhos: function() {
+    	var categoriaId = '#categoria_' + this.instanceId;
+    	var tipoId = 'tipo_' + this.instanceId;
+    	var dom = this.DOM;
+    	
+    	$(categoriaId, dom).on('change', function() {
+    		var opts = [];
+	        switch( $(categoriaId, dom).val() ) {
+	        	case 'sensores':
+	        		opts = ['Dist\u00e2ncia', 'Temperatura', 'Press\u00e3o', 'Luminosidade'];
+	        		break;
+	        	case 'workflows':
+	        		opts = ['Ao iniciar', 'Ao movimentar'];
+	        		break;
+	        	case 'documentos':
+	        		opts = ['Ao criar', 'Ao aprovar'];
+	        		break;
+	        }
+	        
+	        var combo = document.getElementById(tipoId);
+	        $('#' + tipoId, dom).children().remove().end();
+	        
+	        for( var i = 0; i < opts.length; i++ ) {
+	        	var option = document.createElement("option");
+				option.text = opts[i];
+				option.value = opts[i];
+				try {
+					combo.add(option, null);
+				} catch (error) {
+					FLUIGC.toast({
+						title : 'Desculpe', 
+						message : 'Houve um problema no carregamento de alguns itens na p\u00e1gina. Tente novamente em alguns instantes.',
+						type : 'danger',
+						timeout : "4000"
+					});
+					combo.add(option);
+				}
+	        }
+	    });
+    },
+ 
   
     //BIND de eventos
     bindings: {
         local: {
-            'execute': ['click_executeAction']
+            'execute': ['click_executeAction'],
+            'gatilho-add' : [ 'click_gatilhoAdd' ],
+            'acao-add' : [ 'click_acaoAdd' ]
         },
         global: {}
     },
@@ -62,7 +114,158 @@ var widgetFoT = SuperWidget.extend({
             });
             combo.add(option);
         }
-    }
+    },
+    
+    gatilhoAdd: function() {
+    	var nome = $('#nome_' + this.instanceId, this.DOM).val();
+    	var categoria = $('#categoria_' + this.instanceId, this.DOM).val();
+    	var tipo = $('#tipo_' + this.instanceId, this.DOM).val();
+    	
+    	if( !nome || !categoria || !tipo ) {
+    		FLUIGC.toast({
+				title : 'Desculpe', 
+				message : 'Todos os campos s\u00e3o obrigat\u00f3rios.',
+				type : 'danger',
+				timeout : "4000"
+			});
+    	
+    	} else {
+    		
+    		try{
+    			var result = this.saveForm('gatilho', this.formGatilho);
+    			
+    			if (result==null || result.status == 'error'){
+    				throw "Erro ao salvar formul\u00e1rio: " + result.msg;
+    			}
+    			
+    			FLUIGC.toast({
+    				title: 'Sucesso ',
+    				message: 'Cadastro efetuado com sucesso',
+    				type: 'success',
+    				timeout: 4000
+    			});
+    			
+    			this.initFoT();
+    			
+    		} catch (error) {
+        		FLUIGC.toast({
+    				title : 'Desculpe', 
+    				message : 'Erro ao salvar dados',
+    				type : 'danger',
+    				timeout : "4000"
+    			});
+    		}    		
+    		
+    	}
+    },
+    
+    acaoAdd : function() {
+    	
+    	var nome = $('#nomeAc_' + this.instanceId, this.DOM).val();
+    	var tipo = $('#tipoAc_' + this.instanceId, this.DOM).val();
+    	var categoria = $('#categoriaAc_' + this.instanceId, this.DOM).val();
+    	
+    	if( !nome || !tipo || !categoria ) {
+    		FLUIGC.toast({
+				title : 'Desculpe', 
+				message : 'Todos os campos são obrigatósrios.',
+				type : 'danger',
+				timeout : "4000"
+			});
+    	
+    	} else {    	
+    	
+			try{
+				var result = this.saveForm('acao', this.formAcao);
+				
+				if (result==null || result.status == 'error'){
+					throw "Erro ao salvar formulário: " + result.msg;
+				}
+				
+				FLUIGC.toast({
+					title: 'Sucesso ',
+					message: 'Cadastro efetuado com sucesso!',
+					type: 'success',
+					timeout: 4000
+				});
+				
+				this.initFoT();
+				
+			} catch (error) {
+        		FLUIGC.toast({
+    				title : 'Desculpe', 
+    				message : 'Erro ao salvar dados',
+    				type : 'danger',
+    				timeout : "4000"
+    			});
+			}
+    	}
+    },
+    
+	saveForm: function(type, documentId){
+		var $this = this;
+		var result = null;
+		try{
+			var xmlRequest = this.getXmlRequestCreate(type, documentId);
+		
+			var parser=new DOMParser();
+			var dataRequest=parser.parseFromString(xmlRequest,"text/xml");
+			
+			WCMAPI.Create({
+				url: WCMAPI.serverURL + "/webdesk/ECMCardService?wsdl",
+				contentType: "text/xml;charset=UTF-8",
+				dataType: "xml",
+				async: false,
+				data: dataRequest,
+				success: function(data){
+					var xmlResp=parser.parseFromString(data.firstChild.innerHTML,"text/xml");
+					console.log("Documento Publicado: " + xmlResp.getElementsByTagName("documentId")[0].innerHTML);
+					$this.codForm = xmlResp.getElementsByTagName("documentId")[0].innerHTML;
+					result = {status:'success'};
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					result = {status:'error', msg:errorThrown};
+				}
+			});
+			
+		} catch (error) {
+			result = {status:'error', msg:error};
+		}
+		return result;		
+	},
+	
+	getXmlRequestCreate: function(type, documentId){
+		
+		var $this = this;
+		var xml = '<?xml version="1.0" encoding="UTF-8"?>';
+		xml += '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.dm.ecm.technology.totvs.com/">';
+		xml += '<soapenv:Header/><soapenv:Body>';
+		xml += '<ws:create>';
+		xml += '<companyId>' + WCMAPI.organizationId + '</companyId>';
+		xml += '<username></username><password></password>';
+		xml += '<card><item>';
+
+		switch( type ) {
+			case 'gatilho':
+				xml += 	'<cardData><field>nome</field><value>' + $('#nome_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				xml += 	'<cardData><field>tipo</field><value>' + $('#tipo_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				xml += 	'<cardData><field>categoria</field><value>' + $('#categoria_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				break;
+				
+			case 'acao':
+				xml += 	'<cardData><field>nome</field><value>' + $('#nomeAc_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				xml += 	'<cardData><field>tipo</field><value>' + $('#tipoAc_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				xml += 	'<cardData><field>categoria</field><value>' + $('#categoriaAc_' + $this.instanceId, $this.DOM).val() + '</value></cardData>';
+				break;
+		}
+		
+		xml += '<parentDocumentId>' + documentId + '</parentDocumentId>';
+		xml += '</item></card>';
+		xml += '</ws:create>';
+		xml += '</soapenv:Body></soapenv:Envelope>';
+		
+		return xml;		
+	}    
 
 });
 
