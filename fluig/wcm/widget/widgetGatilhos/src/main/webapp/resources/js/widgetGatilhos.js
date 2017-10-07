271c6cc4c79cd65a9a695fc1b86b2802f63f04f1
sp01\claudio.koji <claudio.koji@totvs.com.br>
2017-10-07 13:06:28 -0300

    Parcial

--- fluig/wcm/widget/widgetGatilhos/src/main/webapp/resources/js/widgetGatilhos.js
+++ fluig/wcm/widget/widgetGatilhos/src/main/webapp/resources/js/widgetGatilhos.js
@@ -1,16 +1,24 @@
-var MyWidget = SuperWidget.extend({
-    //variáveis da widget
+var widgetGatilhos = SuperWidget.extend({
+	instanceId: null,
+	widgetURI: null,
     variavelNumerica: null,
     variavelCaracter: null,
 
-    //método iniciado quando a widget é carregada
     init: function() {
+    	var $this = this;
+    	widgetGatilhos.widgetURI = this.widgetURI;
+    	widgetGatilhos.instanceId = this.instanceId
+        
+    	$('#categoria' + this.instanceId, this.DOM).on('change', function() {
+	        alert( $('#categoria' + + this.instanceId).val());    
+	    });
     },
   
     //BIND de eventos
     bindings: {
         local: {
-            'execute': ['click_executeAction']
+            'execute': ['click_executeAction'],
+            'category-changed' : [ 'change_Category' ]
         },
         global: {}
     },
@@ -18,5 +26,6 @@ var MyWidget = SuperWidget.extend({
     executeAction: function(htmlElement, event) {
     }
 
+    
 });
 
