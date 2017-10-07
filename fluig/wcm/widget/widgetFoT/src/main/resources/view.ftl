e6dd07837755e482c3a33a27c19e58a8c9eab3f8
sp01\claudio.koji <claudio.koji@totvs.com.br>
2017-10-07 15:59:38 -0300

    Consistencia widgets

--- fluig/wcm/widget/widgetFoT/src/main/resources/view.ftl
+++ fluig/wcm/widget/widgetFoT/src/main/resources/view.ftl
@@ -1,27 +1,13 @@
 <style>
-	.fot-panels {
-	    display: flex;
-	    width: 100%;
+	.fot-input {
+		display: flex;
+		align-items: center;
 	}
 	
-	.fot-panel {
-	    padding: 10px;
-	    text-align: center;
-	    
-	}
-	
-	.fot-panel.left {
-	    flex-grow: 10;
-	}
-	
-	.fot-panel.middle {
-	    max-width: 80px;
-	    display: flex;
-	    align-items: center;
-	}
-	
-	.fot-panel.right {
-	    flex-grow: 10;
+	.va.center {
+		display: flex;
+		justify-content: center;
+		
 	}
 </style>
 <div id="widgetFoT_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide"
@@ -31,31 +17,33 @@
 	        <h3 class="panel-title">Fluig of Things</h3>
 	    </div>
 	    <div class="panel-body">
-	    	<div class="fot-panels">
-		    	<div class="fot-panel left">
-		    		<div class="form-group">
+	    	<div class="row fot-input">
+	    		<div class="col-sm-5">
+	    			<div class="form-group">
 				        <label for="gatilho">Gatilho</label>
 				        <select class="form-control" id="gatilho_${instanceId}" name="gatilho_${instanceId}">
 				        	<option></option>
 						</select>
 				    </div>
-		    	</div>
-		    	<div class="fot-panel middle">
-		    		<span class="fluigicon fluigicon-arrow-right fluigicon-xl"></span>
-		    	</div>
-		    	<div class="fot-panel right">
-		    		<div class="form-group">
+	    		</div>
+	    		<div class="col-sm-2 va-center" align="center">
+	    			<span class="fluigicon fluigicon-arrow-right fluigicon-xl"></span>
+	    		</div>
+	    		<div class="col-sm-5">
+	    			<div class="form-group">
 				        <label for="acao">A&ccedil;&atilde;o</label>
 				        <select class="form-control" id="acao_${instanceId}" name="acao_${instanceId}">
 				        	<option></option>
 						</select>
 				    </div>
-		    	</div>
-		    </div>
+	    		</div>
+	    	</div>
 		    <div align="center">
 		    	<button type="button" class="btn btn-primary" data-fot-add>Incluir</button>
 		    </div>
 	    </div>
     </div>
 </div>
+
+<script src="/webdesk/vcXMLRPC.js" type="text/javascript"></script>
 
--- fluig/wcm/widget/widgetFoT/src/main/webapp/resources/js/widgetFoT.js
+++ fluig/wcm/widget/widgetFoT/src/main/webapp/resources/js/widgetFoT.js
@@ -6,7 +6,10 @@ var widgetFoT = SuperWidget.extend({
     init: function() {
     	var $this = this;
     	widgetGatilhos.widgetURI = this.widgetURI;
-    	widgetGatilhos.instanceId = this.instanceId
+    	widgetGatilhos.instanceId = this.instanceId;
+
+        this.loadComboboxDS("gatilho_" + this.instanceId, "dsGatilhos", "nome", "nome", null);
+        this.loadComboboxDS("acao_" + this.instanceId, "dsAcoes", "nome", "nome", null);
     },
   
     //BIND de eventos
@@ -18,6 +21,47 @@ var widgetFoT = SuperWidget.extend({
     },
  
     executeAction: function(htmlElement, event) {
+    },
+    
+    loadComboboxDS: function(comboboxName, datasetName, key, value, contraintsMatriz){
+        var constraints = null;
+        if (contraintsMatriz != null){
+            constraints = new Array();
+            var i;
+            for(i=0; i<contraintsMatriz.length;i++){
+                constraints.push(DatasetFactory.createConstraint(contraintsMatriz[i][0], contraintsMatriz[i][1], contraintsMatriz[i][2], ConstraintType.MUST));
+            }
+        }
+        
+        var dataset = DatasetFactory.getDataset(datasetName, null, constraints, null);
+        $('#'+comboboxName+' option').remove();
+        
+        if (dataset.values.length == 0){
+            this.addOptionToSelect(comboboxName, "-1", "Nenhum registro encontrado");
+        }
+        
+        for (var i = 0; i < dataset.values.length; i++) {
+            var row = dataset.values[i];
+            this.addOptionToSelect(comboboxName, row[key], row[value]);
+        }
+    },
+    
+    addOptionToSelect: function(element, value, text){
+        var combo = document.getElementById(element);
+        var option = document.createElement("option");
+        option.text = text;
+        option.value = value;
+        try {
+            combo.add(option, null);
+        } catch (error) {
+            FLUIGC.toast({
+                title : 'Desculpe', 
+                message : 'Houve um problema no carregamento de alguns itens na página. Tente novamente em alguns instantes.',
+                type : 'danger',
+                timeout : "4000"
+            });
+            combo.add(option);
+        }
     }
 
 });
